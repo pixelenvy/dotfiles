58596415a8b8cebe3dc992b90ace1e8f43522240
Nick Harris <nick@pixelenvy.co.uk>
2019-04-25 10:56:54 +0100

    Add Hyper terminal config

---
+++ .hyper.js
@@ -0,0 +1,64 @@
+// Future versions of Hyper may add additional config options,
+// which will not automatically be merged into this file.
+// See https://hyper.is#cfg for all currently supported options.
+
+module.exports = {
+  config: {
+    updateChannel: 'stable',
+    fontSize: 12,
+    fontFamily: 'OperatorMono Nerd Font',
+    fontWeight: 'normal',
+    fontWeightBold: 'bold',
+    lineHeight: 1.2,
+    letterSpacing: 0,
+    cursorColor: 'rgba(248,28,229,0.8)',
+    cursorAccentColor: '#000',
+    cursorShape: 'BEAM',
+    cursorBlink: false,
+    foregroundColor: '#fff',
+    backgroundColor: '#000',
+    selectionColor: 'rgba(248,28,229,0.3)',
+    borderColor: '#333',
+    css: '',
+    termCSS: '',
+    showHamburgerMenu: '',
+    showWindowControls: false,
+    padding: '0',
+    colors: {
+      black: '#000000',
+      red: '#C51E14',
+      green: '#1DC121',
+      yellow: '#C7C329',
+      blue: '#0A2FC4',
+      magenta: '#C839C5',
+      cyan: '#20C5C6',
+      white: '#C7C7C7',
+      lightBlack: '#686868',
+      lightRed: '#FD6F6B',
+      lightGreen: '#67F86F',
+      lightYellow: '#FFFA72',
+      lightBlue: '#6A76FB',
+      lightMagenta: '#FD7CFC',
+      lightCyan: '#68FDFE',
+      lightWhite: '#FFFFFF',
+    },
+    shell: '',
+    shellArgs: ['--login'],
+    env: {},
+    bell: 'SOUND',
+    copyOnSelect: false,
+    defaultSSHApp: true,
+    hyperTransparentVibrancy: {
+      alpha: 0.5, // default 50%
+      vibrancy: 'dark'
+    }
+  },
+  plugins: [
+    'hyper-dracula',
+    'hyperminimal',
+    'hyper-transparent-vibrancy'
+  ],
+  localPlugins: [],
+  keymaps: {
+  },
+};
